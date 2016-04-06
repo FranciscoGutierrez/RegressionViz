0abf3b8ed56f4d9eafa4aaa3e56b3c838742d72f
FranciscoGutierrez <fsalvador23@gmail.com>
2016-04-06 12:15:28 +0200

    added courses data

--- client/regressionviz/regressionviz.js
+++ client/regressionviz/regressionviz.js
@@ -8,11 +8,27 @@ Template.regression.helpers({
     var y2 = 100-(18.51/20)*100;
     return {
       y1: y1,
-      y2: y2
+      y2: y2,
+      a1: y1-5.5,
+      a2: y2-5.5,
+      b1: y1-11,
+      b2: y2-11,
+      c1: y1-16.5,
+      c2: y2-16.5,
+      d1: y1-22,
+      d2: y2-22,
+      aa1: y1 + 5.5,
+      aa2: y2 + 5.5,
+      bb1: y1 + 11,
+      bb2: y2 + 11,
+      cc1: y1 + 16.5,
+      cc2: y2 + 16.5,
+      dd1: y1 + 22,
+      dd2: y2 + 22
     };
   },
-  students() {
-    return 123;
+  courses() {
+    return Courses.find({});
   },
   prediction() {
   }
