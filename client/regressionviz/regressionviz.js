4f7c2b2b2e4e7aa0fd74482813be5a6fe4fbd5e3
FranciscoGutierrez <fsalvador23@gmail.com>
2016-04-15 17:01:35 +0200

    Fixed uncertainty bars

--- client/regressionviz/regressionviz.js
+++ client/regressionviz/regressionviz.js
@@ -10,6 +10,10 @@ Template.regression.helpers({
     var b = Session.get("b");
     var y1 = 0;
     var y2 = 0;
+    var lwrMin = Session.get("lwrMin");
+    var lwrMax = Session.get("lwrMax");
+    var upprMax = Session.get("upprMax");
+    var upprMin = Session.get("upprMin");
     if(a) {
       y1 = 100-(a/20)*100;
       y2 = 100-((b+a)/20)*100;
@@ -17,22 +21,28 @@ Template.regression.helpers({
     return {
       y1: y1,
       y2: y2,
-      a1: y1-5,
-      a2: y2-5,
-      b1: y1-10,
-      b2: y2-10,
-      c1: y1-15,
-      c2: y2-15,
-      d1: y1-20,
-      d2: y2-20,
-      aa1: y1 + 5,
-      aa2: y2 + 5,
-      bb1: y1 + 10,
-      bb2: y2 + 10,
-      cc1: y1 + 15,
-      cc2: y2 + 15,
-      dd1: y1 + 20,
-      dd2: y2 + 20
+      a1: y1+(((a-upprMin)/5)/20)*100,
+      a2: y2+((((a+b)-upprMax)/5)/20)*100,
+      b1: y1+((((a-upprMin)/5)/20)*100)*2,
+      b2: y2+(((((a+b)-upprMax)/5)/20)*100)*2,
+      c1: y1+((((a-upprMin)/5)/20)*100)*3,
+      c2: y2+(((((a+b)-upprMax)/5)/20)*100)*3,
+      d1: y1+((((a-upprMin)/5)/20)*100)*4,
+      d2: y2+(((((a+b)-upprMax)/5)/20)*100)*4,
+      e1: 100-(lwrMin/20)*100,
+      e2: 100-(lwrMax/20)*100,
+      aa1: y1+(((a-lwrMin)/5)/20)*100,
+      aa2: y2+((((a+b)-lwrMax)/5)/20)*100,
+      bb1: y1+((((a-lwrMin)/5)/20)*100)*2,
+      bb2: y2+(((((a+b)-lwrMax)/5)/20)*100)*2,
+      cc1: y1+((((a-lwrMin)/5)/20)*100)*3,
+      cc2: y2+(((((a+b)-lwrMax)/5)/20)*100)*3,
+      dd1: y1+((((a-lwrMin)/5)/20)*100)*4,
+      dd2: y2+(((((a+b)-lwrMax)/5)/20)*100)*4,
+      ee1: 100-(upprMin/20)*100,
+      ee2: 100-(upprMax/20)*100,
+      ratiolwr: Math.abs((((a-lwrMax)/5)/20)*100),
+      ratioupr: Math.abs((((a-upprMax)/5)/20)*100)
     };
   },
   courses() {
@@ -78,37 +88,152 @@ Template.regression.helpers({
   },
   observations() {
     var arr = [];
-    var a = 0;
-    var b = 0;
-    var x = 0;
-    var y = 0;
-    var p = 0;
-    var r = 0;
-    a = Session.get("a");
-    b = Session.get("b");
-    if (a != 0) {
-      for (var i = 0; i<500; i++) {
-        p = (Math.random()*100)/100;
-        c = b + (Math.random() * 10) - 5;
-        r = a + (c * p);
-        x = (p*100).toFixed(1);
-        y = (100-((r/20)*100)).toFixed(1);
-        arr.push({x:x, y:y});
-      }
+    var a = Session.get("a");
+    var b = Session.get("b");
+
+    var lwrMin = Session.get("lwrMin");
+    var lwrMax = Session.get("lwrMax");
+    var upprMax = Session.get("upprMax");
+    var upprMin = Session.get("upprMin");
+
+    var y1 = 100-(a/20)*100;
+    var y2 = 100-((b+a)/20)*100;
+    var a1 = y1+(((a-upprMin)/5)/20)*100;
+    var a2 = y2+((((a+b)-upprMax)/5)/20)*100;
+    var b1 = y1+((((a-upprMin)/5)/20)*100)*2;
+    var b2 = y2+(((((a+b)-upprMax)/5)/20)*100)*2;
+    var c1 = y1+((((a-upprMin)/5)/20)*100)*3;
+    var c2 = y2+(((((a+b)-upprMax)/5)/20)*100)*3;
+    var d1 = y1+((((a-upprMin)/5)/20)*100)*4;
+    var d2 = y2+(((((a+b)-upprMax)/5)/20)*100)*4;
+    var e1 = 100-(lwrMin/20)*100;
+    var e2 = 100-(lwrMax/20)*100;
+    var aa1 = y1+(((a-lwrMin)/5)/20)*100;
+    var aa2 = y2+((((a+b)-lwrMax)/5)/20)*100;
+    var bb1 = y1+((((a-lwrMin)/5)/20)*100)*2;
+    var bb2 = y2+(((((a+b)-lwrMax)/5)/20)*100)*2;
+    var cc1 = y1+((((a-lwrMin)/5)/20)*100)*3;
+    var cc2 = y2+(((((a+b)-lwrMax)/5)/20)*100)*3;
+    var dd1 = y1+((((a-lwrMin)/5)/20)*100)*4;
+    var dd2 = y2+(((((a+b)-lwrMax)/5)/20)*100)*4;
+    var ee1 = 100-(upprMin/20)*100;
+    var ee2 = 100-(upprMax/20)*100;
+
+
+    for (var i = 0; i<10; i++) {
+      var x = (Math.random()*100).toFixed(1);
+      var y = (Math.random()*100).toFixed(1);
+      arr.push({x:x, y:y});
     }
+
+    for (var i = 0; i<200; i++) {
+      var x = (Math.random()*100).toFixed(1);
+      var z = x/100 + _.random(0, 10)/100;
+      var r = a + (b * z);
+      arr.push({x:x, y:100-((r/20)*100)});
+    }
+
+    for (var i = 0; i<100; i++) {
+      var x = (Math.random()*100).toFixed(1);
+      var z = x/100 + _.random(10,20)/100;
+      var r = a + (b * z);
+      arr.push({x:x, y:100-((r/20)*100)});
+    }
+
+    for (var i = 0; i<50; i++) {
+      var x = (Math.random()*100).toFixed(1);
+      var z = x/100 + _.random(20, 30)/100;
+      var r = a + (b * z);
+      arr.push({x:x, y:100-((r/20)*100)});
+    }
+
+    for (var i = 0; i<25; i++) {
+      var x = (Math.random()*100).toFixed(1);
+      var z = x/100 + _.random(30, 40)/100;
+      var r = a + (b * z);
+      arr.push({x:x, y:100-((r/20)*100)});
+    }
+
+
+    for (var i = 0; i<200; i++) {
+      var x = (Math.random()*100).toFixed(1);
+      var z = x/100 + _.random(0, -10)/100;
+      var r = a + (b * z);
+      arr.push({x:x, y:100-((r/20)*100)});
+    }
+
+    for (var i = 0; i<100; i++) {
+      var x = (Math.random()*100).toFixed(1);
+      var z = x/100 + _.random(-10,-20)/100;
+      var r = a + (b * z);
+      arr.push({x:x, y:100-((r/20)*100)});
+    }
+
+    for (var i = 0; i<50; i++) {
+      var x = (Math.random()*100).toFixed(1);
+      var z = x/100 + _.random(-20, -30)/100;
+      var r = a + (b * z);
+      arr.push({x:x, y:100-((r/20)*100)});
+    }
+
+    for (var i = 0; i<25; i++) {
+      var x = (Math.random()*100).toFixed(1);
+      var z = x/100 + _.random(-30, -40)/100;
+      var r = a + (b * z);
+      arr.push({x:x, y:100-((r/20)*100)});
+    }
+
     return arr;
   },
   details() {
-    var p = parseFloat(Session.get("p"));
-    var u = (p+4.91).toFixed(2);
-    var l = (p-4.91).toFixed(2);
-    if(l<0) l = 0.00;
-    var g = (u-10)*10;
-    var r = (7-l)*10;
+    var maxFit  = Session.get("maxFit");
+    var minFit  = Session.get("minFit");
+    var lwrMin  = Session.get("lwrMin");
+    var lwrMax  = Session.get("lwrMax");
+    var upprMax = Session.get("upprMax");
+    var upprMin = Session.get("upprMin");
+    var prediction = parseFloat(Session.get("p"));
+    var p = parseFloat(Session.get("performance"));
+    var u = (upprMin+((upprMax-upprMin)*(p/100))).toFixed(2);
+    var l = (lwrMin +((lwrMax-lwrMin)*(p/100))).toFixed(2);
+
+    var lwr = Session.get("lwr");
+    var upr = Session.get("upr");
+    var ratioUpr = Math.abs((((prediction - upr)/5)/20)*100);
+    var ratioLwr = Math.abs((((prediction - lwr)/5)/20)*100);
+
+    if(u > 20) u = 20;
+    if(l < 0)  l = 0;
+
+    var g = ((prediction+4.91)-10)*10;
+    var r = (7-(prediction-4.91))*10;
     if(g<0) g = 0;
     var y = 100-(g+r);
-    return {lwr: l, upr: u, red: r, yellow: y, green:g};
+    Session.set("lwr",l);
+    Session.set("upr",u);
+
+
+    return {
+      lwr: l,
+      upr: u,
+      red: r,
+      yellow: y,
+      green:g,
+      ua1: ratioUpr*5,
+      ua2: ratioUpr*4,
+      ua3: ratioUpr*3,
+      ua4: ratioUpr*2,
+      ua5: ratioUpr+ratioLwr,
+      ua6: ratioLwr*2,
+      ua7: ratioLwr*3,
+      ua8: ratioLwr*4,
+      ua9: ratioLwr*5
+    };
+  },
+  size() {
+    return Session.get("size");
   }
+
 });
 
 Template.regression.events({
@@ -266,6 +391,19 @@ Template.regression.rendered = function () {
     $(".main-circle").css("fill","#8c3d5e");
     if(val<0) val=0;
     Session.set("performance",val);
+    var size = _.random(2, 5);
+    $(".udot").css("opacity","0");
+    $(".ua1:lt("+_.random(0, 2)+")").css("opacity","1");
+    $(".ua2:lt("+_.random(1, 2)+")").css("opacity","1");
+    $(".ua3:lt("+_.random(1, 4)+")").css("opacity","1");
+    $(".ua4:lt("+_.random(1, 4)+")").css("opacity","1");
+
+    $(".ua5:lt("+size+")").css("opacity","1");
+    Session.set("size", size);
+    $(".ua6:lt("+_.random(1, 4)+")").css("opacity","1");
+    $(".ua7:lt("+_.random(1, 4)+")").css("opacity","1");
+    $(".ua8:lt("+_.random(1, 2)+")").css("opacity","1");
+    $(".ua9:lt("+_.random(0, 2)+")").css("opacity","1");
 
   }).on('change', function (ev, val) {
     if(val<0) val=0;
