2e962511e663d9a57325e3a15ee7c2226b4d7dda
FranciscoGutierrez <fsalvador23@gmail.com>
2016-04-20 16:16:58 +0200

    Fixed visualization

--- client/regressionviz/regressionviz.js
+++ client/regressionviz/regressionviz.js
@@ -4,53 +4,73 @@ Grades   = new Meteor.Collection('studentscourses');
 Meteor.subscribe("this_courses");
 /*Publish - subscribe */
 
-Session.setDefault("a", 2);
-Session.setDefault("b", 12);
-Session.setDefault("p", 11.70);
-Session.setDefault("performance", 80);
+Session.setDefault("a", 0);
+Session.setDefault("b", 0);
+Session.setDefault("p", 0);
+Session.setDefault("performance", 0);
+Session.setDefault("lwrMin",0);
+Session.setDefault("lwrMax",0);
+Session.setDefault("upprMax",0);
+Session.setDefault("upprMin",0);
 
 Template.regression.helpers({
   regressionLine() {
     var a = Session.get("a");
     var b = Session.get("b");
-    var y1 = 0;
-    var y2 = 0;
+    var y1 = 400-(a/20)*400;
+    var y2 = 400-((b+a)/20)*400;
     var lwrMin = Session.get("lwrMin");
     var lwrMax = Session.get("lwrMax");
     var upprMax = Session.get("upprMax");
     var upprMin = Session.get("upprMin");
-    if(a) {
-      y1 = 400-(a/20)*400;
-      y2 = 400-((b+a)/20)*400;
-
-      console.log(a);
-      console.log(b+a);
-    }
+    var ratioUpr1 = (((upprMin-a)/5)/20)*400;
+    var ratioUpr2 = (((upprMax-(b+a))/5)/20)*400;
+    /* Middle */
+    var ratioLwr1 = (((a-lwrMin)/5)/20)*400;
+    var ratioLwr2 = ((((b+a)-lwrMax)/5)/20)*400;
     return {
-      y1: y1,
-      y2: y2,
-      a1: y1+(((a-upprMin)/5)/20)*400,
-      a2: y2+((((a+b)-upprMax)/5)/20)*400,
-      b1: y1+((((a-upprMin)/5)/20)*400)*2,
-      b2: y2+(((((a+b)-upprMax)/5)/20)*400)*2,
-      c1: y1+((((a-upprMin)/5)/20)*400)*3,
-      c2: y2+(((((a+b)-upprMax)/5)/20)*400)*3,
-      d1: y1+((((a-upprMin)/5)/20)*100)*4,
-      d2: y2+(((((a+b)-upprMax)/5)/20)*400)*4,
-      e1: 400-(lwrMin/20)*400,
-      e2: 400-(lwrMax/20)*400,
-      aa1: y1+(((a-lwrMin)/5)/20)*400,
-      aa2: y2+((((a+b)-lwrMax)/5)/20)*400,
-      bb1: y1+((((a-lwrMin)/5)/20)*400)*2,
-      bb2: y2+(((((a+b)-lwrMax)/5)/20)*400)*2,
-      cc1: y1+((((a-lwrMin)/5)/20)*400)*3,
-      cc2: y2+(((((a+b)-lwrMax)/5)/20)*400)*3,
-      dd1: y1+((((a-lwrMin)/5)/20)*400)*4,
-      dd2: y2+(((((a+b)-lwrMax)/5)/20)*400)*4,
-      ee1: 400-(upprMin/20)*400,
-      ee2: 400-(upprMax/20)*400,
-      ratiolwr: Math.abs((((a-lwrMax)/5)/20)*100),
-      ratioupr: Math.abs((((a-upprMax)/5)/20)*100)
+      y1:y1,
+      y2:y2,
+      a1:y1,
+      a2:y2,
+      ra1:y1-ratioUpr1,
+      ra2:y2-ratioUpr2,
+      b1:y1-ratioUpr1,
+      b2:y2-ratioUpr2,
+      rb1:y1-(ratioUpr1*2),
+      rb2:y2-(ratioUpr2*2),
+      c1:y1-(ratioUpr1*2),
+      c2:y2-(ratioUpr2*2),
+      rc1:y1-(ratioUpr1*3),
+      rc2:y2-(ratioUpr2*3),
+      d1:y1-(ratioUpr1*3),
+      d2:y2-(ratioUpr2*3),
+      rd1:y1-(ratioUpr1*4),
+      rd2:y2-(ratioUpr2*4),
+      e1:y1-(ratioUpr1*4),
+      e2:y2-(ratioUpr2*4),
+      re1:y1-(ratioUpr1*5),
+      re2:y2-(ratioUpr2*5),
+      aa1:y1,
+      aa2:y2,
+      raa1:y1+ratioLwr1,
+      raa2:y2+ratioLwr2,
+      bb1:y1+ratioLwr1,
+      bb2:y2+ratioLwr2,
+      rbb1:y1+(ratioLwr1*2),
+      rbb2:y2+(ratioLwr2*2),
+      cc1:y1+(ratioLwr1*2),
+      cc2:y2+(ratioLwr2*2),
+      rcc1:y1+(ratioLwr1*3),
+      rcc2:y2+(ratioLwr2*3),
+      dd1:y1+(ratioLwr1*3),
+      dd2:y2+(ratioLwr2*3),
+      rdd1:y1+(ratioLwr1*4),
+      rdd2:y2+(ratioLwr2*4),
+      ee1:y1+(ratioLwr1*4),
+      ee2:y2+(ratioLwr2*4),
+      ree1:y1+(ratioLwr1*5),
+      ree2:y2+(ratioLwr2*5)
     };
   },
   courses() {
@@ -146,39 +166,38 @@ Template.regression.helpers({
     var upprMin = Session.get("upprMin");
     var prediction = parseFloat(Session.get("p"));
     var p = parseFloat(Session.get("performance"));
-    var u = (upprMin+((upprMax-upprMin)*(p/100))).toFixed(2);
-    var l = (lwrMin +((lwrMax-lwrMin)*(p/100))).toFixed(2);
+    var u = (upprMin+((upprMax-upprMin)*(p/100))).toFixed(1);
+    var l = (lwrMin +((lwrMax-lwrMin)*(p/100))).toFixed(1);
 
     var lwr = Session.get("lwr");
     var upr = Session.get("upr");
-    var ratioUpr = Math.abs((((prediction - upr)/5)/20)*100);
-    var ratioLwr = Math.abs((((prediction - lwr)/5)/20)*100);
+    var ratioUpr = (((upr - prediction)/5)/20)*100;
+    var ratioLwr = (((prediction - lwr)/5)/20)*100;
 
     if(u > 20) u = 20;
     if(l < 0)  l = 0;
-
     var g = ((prediction+4.91)-10)*10;
     var r = (7-(prediction-4.91))*10;
     if(g<0) g = 0;
     var y = 100-(g+r);
     Session.set("lwr",l);
     Session.set("upr",u);
-
     return {
       lwr: l,
       upr: u,
       red: r,
       yellow: y,
-      green:g,
+      green:  g,
       ua1: ratioLwr*4,
       ua2: ratioLwr*4,
       ua3: ratioLwr*4,
       ua4: ratioLwr*4,
-      ua5: (ratioUpr+ratioLwr)*4,
+      ua5: ratioLwr*4,
       ua6: ratioUpr*4,
       ua7: ratioUpr*4,
       ua8: ratioUpr*4,
-      ua9: ratioUpr*4
+      ua9: ratioUpr*4,
+      ua10: ratioUpr*4
     };
   },
   size() {
@@ -195,16 +214,14 @@ Template.regression.events({
       return this.value;
     }).get();
     if (arr.length <= 0) {
-      $(".uline").fadeOut(function(){
-        Session.set("a",0);
-        Session.set("b",0);
-        Session.set("maxFit",0);
-        Session.set("minFit",0);
-        Session.set("lwrMin",0);
-        Session.set("lwrMax",0);
-        Session.set("upprMax",0);
-        Session.set("upprMin",0);
-      });
+      Session.set("a",0);
+      Session.set("b",0);
+      Session.set("maxFit",0);
+      Session.set("minFit",0);
+      Session.set("lwrMin",0);
+      Session.set("lwrMax",0);
+      Session.set("upprMax",0);
+      Session.set("upprMin",0);
     } else {
       var courses = Courses.find({ "_id":{ $in: arr  }}).fetch();
       if (arr.length < 11) Meteor.subscribe("grades", arr);
@@ -234,7 +251,6 @@ Template.regression.events({
       Session.set("lwrMax",lwrMax/courses.length);
       Session.set("upprMax",upprMax/courses.length);
       Session.set("upprMin",upprMin/courses.length);
-      $(".uline").fadeIn();
     }
   },
   "change .performance-slider"(event, instance) {
@@ -269,12 +285,9 @@ Template.regression.events({
     Session.set("lwrMax",lwrMax/courses.length);
     Session.set("upprMax",upprMax/courses.length);
     Session.set("upprMin",upprMin/courses.length);
-    $(".uline").fadeIn();
     $(".observations").css("fill", "#b0b0b0");
   },
   "click .unselect-all"(event, instance) {
-    $(".uline").fadeOut();
-    $(".observations").fadeOut();
     instance.$(".ccb").prop("checked",false);
     Session.set("a",0);
     Session.set("b",0);
@@ -343,20 +356,13 @@ Template.regression.rendered = function () {
     $(".main-circle").css("fill","#8c3d5e");
     if(val<0) val=0;
     Session.set("performance",val);
-    var size = _.random(2, 5);
-    $(".udot").css("opacity","0");
-    $(".ua1:lt("+_.random(0, 2)+")").css("opacity","1");
-    $(".ua2:lt("+_.random(1, 2)+")").css("opacity","1");
-    $(".ua3:lt("+_.random(1, 4)+")").css("opacity","1");
-    $(".ua4:lt("+_.random(1, 4)+")").css("opacity","1");
-
+    Session.get("upr");
+    Session.get("p");
+    Session.get("lwr");
+    var size = _.random(8, 8);
     $(".ua5:lt("+size+")").css("opacity","1");
+    /******/
     Session.set("size", size);
-    $(".ua6:lt("+_.random(1, 4)+")").css("opacity","1");
-    $(".ua7:lt("+_.random(1, 4)+")").css("opacity","1");
-    $(".ua8:lt("+_.random(1, 2)+")").css("opacity","1");
-    $(".ua9:lt("+_.random(0, 2)+")").css("opacity","1");
-
   }).on('change', function (ev, val) {
     if(val<0) val=0;
     Session.set("performance",val);
